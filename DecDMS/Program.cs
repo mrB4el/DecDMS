7ba51750d63e41a97877d99fb2a6754fc2e708da
mrB4el <mrb4el@hotmail.com>
2018-06-20 14:51:31 +0700

    first devel commit

--- DecDMS/Program.cs
+++ DecDMS/Program.cs
@@ -4,9 +4,14 @@ namespace DecDMS
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task MainAsync(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var host = new HostBuilder().Build();
+
+            await host.RunAsync();
+
         }
     }
 }
